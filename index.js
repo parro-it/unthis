d6d17f1be7b288b930bdd10853bd4d391b49e7cb
Andrea Parodi <andrea@parro.it>
2017-10-26 16:36:14 +0200

    moved from other repo

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-export default function unthis() {
-  return 42;
+import curry from "curry";
+
+const includeProp = (object, exclude, include) => prop =>
+  prop !== "constructor" &&
+  typeof object[prop] === "function" &&
+  (include === null || include.includes(prop)) &&
+  !exclude.includes(prop);
+
+export function unthis(method) {
+  if (typeof method !== "function") {
+    throw new TypeError("method argument shall be a function");
+  }
+
+  const fn = (...args) => method.apply(args.slice(-1)[0], args.slice(0, -1));
+
+  const ret = curry.to(method.length + 1, fn);
+  Object.defineProperty(ret, "name", {
+    value: method.name,
+    configurable: true
+  });
+
+  return ret;
 }
+
+export const unthisAll = (object, { exclude = [], include = null } = {}) =>
+  Object.assign(
+    ...Object.getOwnPropertyNames(Array.prototype)
+      .filter(includeProp(object, exclude, include))
+      .map(prop => ({ [prop]: unthis(object[prop]) }))
+  );
--- test.js
+++ test.js
@@ -1,6 +1,87 @@
 import test from "ava";
-import unthis from ".";
+import map from "map-obj";
+import filter from "filter-obj";
 
-test("exports a function", t => {
-  t.is(typeof unthis, "function");
+import { unthis, unthisAll } from ".";
+
+const arrayResult = {
+  toString: "toString",
+  toLocaleString: "toLocaleString",
+  join: "join",
+  pop: "pop",
+  push: "push",
+  reverse: "reverse",
+  shift: "shift",
+  unshift: "unshift",
+  slice: "slice",
+  splice: "splice",
+  sort: "sort",
+  filter: "filter",
+  forEach: "forEach",
+  some: "some",
+  every: "every",
+  map: "map",
+  indexOf: "indexOf",
+  lastIndexOf: "lastIndexOf",
+  reduce: "reduce",
+  reduceRight: "reduceRight",
+  copyWithin: "copyWithin",
+  find: "find",
+  findIndex: "findIndex",
+  fill: "fill",
+  includes: "includes",
+  entries: "entries",
+  keys: "keys",
+  concat: "concat"
+};
+
+const obj = {
+  nameIt(name) {
+    this.name = name;
+  },
+  surnameIt(surname) {
+    this.surname = surname;
+  }
+};
+
+test("return a function that accept this as last arg", t => {
+  const nameIt = unthis(obj.nameIt);
+  nameIt("named", obj);
+  t.is(obj.name, "named");
+});
+
+test("return a function with same name as original", t => {
+  const fnName = unthis(obj.nameIt).name;
+  t.is(fnName, "nameIt");
+});
+
+test("returned function is curried", t => {
+  const filter = unthis([].filter);
+  const even = n => n % 2 === 0;
+  const filterEven = filter(even);
+  t.deepEqual(filterEven([1, 2, 3, 4]), [2, 4]);
+});
+
+test("unthis all methods of an object", t => {
+  const obj = unthisAll(Array.prototype);
+  t.deepEqual(map(obj, (k, v) => [k, v.name]), arrayResult);
+});
+
+test("exclude some methods with exclude option", t => {
+  const obj = unthisAll(Array.prototype, { exclude: ["toString"] });
+  t.deepEqual(
+    map(obj, (k, v) => [k, v.name]),
+    filter(arrayResult, k => k !== "toString")
+  );
+});
+
+test("include only some methods with include option", t => {
+  const obj = unthisAll(Array.prototype, { include: ["toString"] });
+  t.deepEqual(map(obj, (k, v) => [k, v.name]), { toString: "toString" });
+});
+
+test("throws if argument is not a function", t => {
+  const err = t.throws(() => unthis(""));
+  t.true(err instanceof TypeError);
+  t.is(err.message, "method argument shall be a function");
 });
