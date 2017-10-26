919eeaf953c16005f05279e5efd1f1d06608674b
Andrea Parodi <andrea@parro.it>
2017-10-26 17:26:11 +0200

    First public release

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
-const unthis = require("unthis");
+const unthis = require("unthis").unthis;
 
-console.log({ unthis });
+const match = unthis(String.prototype.match);
+const matchFinalS = match(/s$/);
+
+console.log(matchFinalS("words") !== null); // --> true
+console.log(matchFinalS("lollo") !== null); // --> false
--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const includeProp = (object, exclude, include) => prop =>
   (include === null || include.includes(prop)) &&
   !exclude.includes(prop);
 
+/**
+ * Given an object method, unthis return a function that
+ * accept all original arguments plus one.
+ * It forward all arguments to the method, and uses the
+ * last argument as `this` context for it.
+ *
+ * @param  {Function} method The original method to transform
+ * @return {Function} The transformed function
+ */
 export function unthis(method) {
   if (typeof method !== "function") {
     throw new TypeError("method argument shall be a function");
@@ -22,6 +31,18 @@ export function unthis(method) {
   return ret;
 }
 
+/**
+ * Given an object, unthisAll apply `unthis` to all its
+ * methods, and return an object containing all resulting functions.
+ *
+ * By default, all object methods are extracted. Otherwise, you can
+ * also specify which method to include or exclude.
+ *
+ * @param  {Object} object The object containing methods to transform.
+ * @param  {Array} exclude An array containing name of methods to transform.
+ * @param  {Array} include An array containing name of methods to exclude from transform.
+ * @return {Object} An object with all transformed methods.
+ */
 export const unthisAll = (object, { exclude = [], include = null } = {}) =>
   Object.assign(
     ...Object.getOwnPropertyNames(Array.prototype)
